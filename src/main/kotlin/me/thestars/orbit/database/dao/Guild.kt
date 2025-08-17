2ffa09651b76b79a6e6a6c9f5a21d29a715f0508
0xviny <vgalvaomarcelino@gmail.com>
2025-08-16 23:06:16 -0300

    feat: adicionado novos tipos de database e novas tabelas.

--- src/main/kotlin/me/thestars/orbit/database/dao/Guild.kt
+++ src/main/kotlin/me/thestars/orbit/database/dao/Guild.kt
@@ -1,15 +1,40 @@
 package me.thestars.orbit.database.dao
 
+import me.thestars.orbit.database.table.AutoPings
 import me.thestars.orbit.database.table.Guilds
-import org.jetbrains.exposed.dao.LongEntity
-import org.jetbrains.exposed.dao.LongEntityClass
+import me.thestars.orbit.database.table.OrbitConnections
+import me.thestars.orbit.database.table.Premiums
+import me.thestars.orbit.database.table.UserGuildSettings
+import org.jetbrains.exposed.dao.UUIDEntity
+import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
+import java.util.UUID
 
-class Guild(id: EntityID<Long>) : LongEntity(id) {
-    companion object : LongEntityClass<Guild>(Guilds) {
-        fun getOrInsert(id: Long) = Guild.findById(id) ?: Guild.new(id) {}
-    }
+class Guild(id: EntityID<UUID>) : UUIDEntity(id) {
+    companion object : UUIDEntityClass<Guild>(Guilds)
 
-    val guildId = this.id.value
-    val prefix by Guilds.prefix
+    var prefix by Guilds.prefix
+    var cases by Guilds.cases
+    var language by Guilds.language
+    var createdAt by Guilds.createdAt
+
+    val orbitConnections by OrbitConnection referrersOn OrbitConnections.guild
+    val premium by Premium referrersOn Premiums.guild
+}
+
+
+class UserGuildSetting(id: EntityID<UUID>) : UUIDEntity(id) {
+    companion object : UUIDEntityClass<UserGuildSetting>(UserGuildSettings)
+
+    var userId by UserGuildSettings.userId
+    var guildId by UserGuildSettings.guildId
+    var atmBorder by UserGuildSettings.atmBorder
+}
+
+class AutoPing(id: EntityID<UUID>) : UUIDEntity(id) {
+    companion object : UUIDEntityClass<AutoPing>(AutoPings)
+
+    var guildId by AutoPings.guildId
+    var channelId by AutoPings.channelId
+    var pingId by AutoPings.pingId
 }
--- src/main/kotlin/me/thestars/orbit/utils/Constants.kt
+++ src/main/kotlin/me/thestars/orbit/utils/Constants.kt
@@ -3,3 +3,12 @@ package me.thestars.orbit.utils
 object Constants {
     const val DEFAULT_COLOR = 0x5865f2
 }
+
+object Enums {
+    enum class AlertType { Reply, Backup, System, TeamInvite, Gift, Mention }
+    enum class ModType { Owner, Admin }
+    enum class ModerationRuleType { EXACT_WORD, REGEX, WILDCARD }
+    enum class ModerationAction { DELETE_MESSAGE, WARN_USER, TIMEOUT_USER }
+    enum class LogType { MessageDeleted, MessageEdited, MessageSuspect, MessageGrave, BlockedWord, MessageConfirmed, MessageRejected }
+    enum class CosmeticType { BANNER, BORDER, BADGE }
+}
